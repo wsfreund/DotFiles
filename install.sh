ab8c6e742a030e25db47b2b3b6ee938060268b40
Werner Freund <wsfreund@gmail.com>
2016-07-25 08:31:18 -0300

    Added oh-my-zsh installation (still no zsh installation)

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ if test $(basename $SHELL) != "zsh"; then
   chsh -s $(which zsh) || return $chsherrno;
 fi
 
+if ! test -e $HOME/.oh-my-zsh; then
+  git clone git@github.com:wsfreund/oh-my-zsh.git $HOME/.oh-my-zsh
+fi
+
 if test -e $HOME/.zshrc -a \! -e $HOME/.zshrc_bkg; then
   mv $HOME/.zshrc $HOME/.zshrc_bkg || return $mvzshrcerrno;
 fi
@@ -33,11 +37,11 @@ if test -e $HOME/.vim -a \! -e $HOME/.vim_bkg; then
   mv $HOME/.vim $HOME/.vim_bkg || return $mvvimerno;
 fi
 
-ln -s $HOME/DotFiles/zshrc $HOME/.zshrc
 ln -s $HOME/DotFiles/dircolors.256dark $HOME/.dircolors.256dark
 ln -s $HOME/DotFiles/shell_prompt.sh $HOME/.shell_prompt.sh
 ln -s $HOME/DotFiles/shell_prompt_no_pl.sh $HOME/.shell_prompt_no_pl.sh
 
+# It is missing the +lua vim installation
 ln -s $HOME/DotFiles/vimrc $HOME/.vimrc
 
 ln -s $HOME/DotFiles/vim $HOME/.vim
