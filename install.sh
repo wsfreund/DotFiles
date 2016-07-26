0600dae3c25968101c782ae192d20e96247c08cb
Werner Freund <wsfreund@gmail.com>
2016-07-26 12:19:35 -0300

    Added ssh config

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 
 ## error codes
-chsherrno=1;
-mvzshrcerrno=2;
-mvvimrcerno=3;
-mvvimerno=4;
 
 if test $(basename $SHELL) != "zsh"; then 
   chsh -s $(which zsh) || return $chsherrno;
@@ -13,26 +9,35 @@ if ! test -e $HOME/.oh-my-zsh; then
   git clone https://github.com/wsfreund/oh-my-zsh.git $HOME/.oh-my-zsh
 fi
 
+if test -e $HOME/.ssh/config; then
+  mv $HOME/.ssh/config $HOME/.ssh/config_bkg || return 1;
+fi
+
 if test -e $HOME/.zshrc -a \! -e $HOME/.zshrc_bkg; then
-  mv $HOME/.zshrc $HOME/.zshrc_bkg || return $mvzshrcerrno;
+  mv $HOME/.zshrc $HOME/.zshrc_bkg || return 1;
+fi
+
+if test -e $HOME/.zshrc -a \! -e $HOME/.zshrc_bkg; then
+  mv $HOME/.zshrc $HOME/.zshrc_bkg || return 1;
 fi
 
 if test -e $HOME/.dircolors.256dark -a \! -e $HOME/.dircolors.256dark_bkg; then
-  mv $HOME/.dircolors.256dark $HOME/.dircolors.256dark_bkg || return $mvzshrcerrno;
+  mv $HOME/.dircolors.256dark $HOME/.dircolors.256dark_bkg || return 1;
 fi
 
 if test -e $HOME/.shell_prompt.sh -a \! -e $HOME/.shell_prompt_bkg.sh; then
-  mv $HOME/.shell_prompt.sh $HOME/.shell_prompt_bkg.sh || return $mvzshrcerrno;
+  mv $HOME/.shell_prompt.sh $HOME/.shell_prompt_bkg.sh || return 1;
 fi
 
 if test -e $HOME/.vimrc -a \! -e $HOME/.vimrc_bkg; then
-  mv $HOME/.vimrc $HOME/.vimrc_bkg || return $mvvimrcerno;
+  mv $HOME/.vimrc $HOME/.vimrc_bkg || return 1;
 fi
 
 if test -e $HOME/.vim -a \! -e $HOME/.vim_bkg; then
-  mv $HOME/.vim $HOME/.vim_bkg || return $mvvimerno;
+  mv $HOME/.vim $HOME/.vim_bkg || return 1;
 fi
 
+ln -s $HOME/DotFiles/config $HOME/.ssh/config
 ln -s $HOME/DotFiles/zshrc $HOME/.zshrc
 ln -s $HOME/DotFiles/dircolors.256dark $HOME/.dircolors.256dark
 ln -s $HOME/DotFiles/shell_prompt.sh $HOME/.shell_prompt.sh
