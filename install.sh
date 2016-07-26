7a2eda6f992896f667c608af4e14a96da4956805
wsfreund <wsfreund@cern.ch>
2016-07-26 17:23:40 +0200

    Added screenrc

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 
-## error codes
 
-if test $(basename $SHELL) != "zsh"; then 
-  chsh -s $(which zsh) || return $chsherrno;
-fi
+#if test $(basename $SHELL) != "zsh"; then 
+#  chsh -s $(which zsh) || return $chsherrno;
+#fi
 
 if ! test -e $HOME/.oh-my-zsh; then
   git clone https://github.com/wsfreund/oh-my-zsh.git $HOME/.oh-my-zsh
@@ -11,6 +10,10 @@ fi
 
 if test -e $HOME/.ssh/config; then
   mv $HOME/.ssh/config $HOME/.ssh/config_bkg || return 1;
+fi
+
+if test -e $HOME/.screenrc; then
+  mv $HOME/.screenrc $HOME/.screenrc_bkg || return 1;
 fi
 
 if test -e $HOME/.zshrc -a \! -e $HOME/.zshrc_bkg; then
@@ -38,6 +41,7 @@ if test -e $HOME/.vim -a \! -e $HOME/.vim_bkg; then
 fi
 
 ln -s $HOME/DotFiles/config $HOME/.ssh/config
+ln -s $HOME/DotFiles/screenrc $HOME/.screenrc
 ln -s $HOME/DotFiles/zshrc $HOME/.zshrc
 ln -s $HOME/DotFiles/dircolors.256dark $HOME/.dircolors.256dark
 ln -s $HOME/DotFiles/shell_prompt.sh $HOME/.shell_prompt.sh
