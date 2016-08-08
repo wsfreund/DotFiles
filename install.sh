64702db6094273326467b9d651b2b8701bb77f52
Werner Freund <wsfreund@gmail.com>
2016-08-08 03:30:51 -0300

    Changed install.sh to better handle back-ups

--- install.sh
+++ install.sh
@@ -1,6 +1,71 @@
+#!/bin/bash
+# We use /bin/bash as it should be available at most of the systems
 
+readlink_f(){
+# Re-implement recursive readlink in case system uses BSD readlink
+  while [ -L "$1" ]; do
+    $1="`readlink "$1"`"
+  done
+  echo "$1"
+}
 
-if test $(hostname -d) = "lps.ufrj.br"; then
+hostname_d(){
+# Re-implements hostname domain if using BSD
+  var=`hostname -f`
+  echo "${var#*.}"
+}
+
+# Get the backup folder
+bkg_date=`date +%Y-%m-%d:%H:%M:%S`
+bkg_folder="$HOME/DotFiles/backups/$bkg_date"
+
+# and this host domain
+host_domain="`hostname -d 2> /dev/null || hostname_d`"
+
+get_dotfile(){
+  base_1=`basename $1`
+  echo "$HOME/DotFiles/${base_1#.}"
+}
+
+backup(){
+  base_1=`basename $1`
+  [ -n "$2" ] && $2="$HOME/DotFiles/$base_1"
+  if [ -e "$1" ]; then
+    file=`readlink -f "$1" 2>/dev/null || readlink_f "$1"`
+    [ "$file" != "$(get_dotfile "$1")" ] && { [ \! -d "$bkg_folder" ] && \
+      mkdir -p "$bkg_folder" || true && mv $1 "$bkg_folder/" && \
+      echo "Created back-up of your old configuration file(s) '$1' on '$bkg_folder'."; } || \
+      echo "No need to create back-up for '$1', you are already using DotFiles configuration."
+  elif [ -L "$1" ]; then
+    echo "Removing bad link '$1'." && rm "$1"
+  fi
+}
+
+link_dotfile(){
+  ln -s "$(get_dotfile $1)" "$1"
+}
+
+files=(\
+       "$HOME/.ssh/config" \
+       "$HOME/.screenrc" \
+       "$HOME/.zshrc" \
+       "$HOME/.dircolors.256dark" \
+       "$HOME/.shell_prompt.sh" \
+       "$HOME/.vimrc" \
+       "$HOME/.vim" \
+      )
+
+echo $files
+
+if test "$host_domain" = "lps.ufrj.br"; then
+  ${files[${#files[*]}+1]}=".bashrc"
+fi
+
+for file in ${files[@]}; do
+  backup "$file" && link_dotfile "$file"
+done
+
+if test "$host_domain" = "lps.ufrj.br"; then
   if ! type zsh > /dev/null 2> /dev/null; then
     # Zsh
     zsh_tgz_file=$HOME/DotFiles/zsh.tgz
@@ -32,58 +97,7 @@ if ! test -e $HOME/.oh-my-zsh; then
   git clone https://github.com/wsfreund/oh-my-zsh.git $HOME/.oh-my-zsh
 fi
 
-if test -e $HOME/.ssh/config; then
-  mv $HOME/.ssh/config $HOME/.ssh/config_bkg || return 1;
-fi
-
-if test -e $HOME/.screenrc; then
-  mv $HOME/.screenrc $HOME/.screenrc_bkg || return 1;
-fi
-
-if test -e $HOME/.zshrc -a \! -e $HOME/.zshrc_bkg; then
-  mv $HOME/.zshrc $HOME/.zshrc_bkg || return 1;
-fi
-
-if test -e $HOME/.zshrc -a \! -e $HOME/.zshrc_bkg; then
-  mv $HOME/.zshrc $HOME/.zshrc_bkg || return 1;
-fi
-
-if test -e $HOME/.dircolors.256dark -a \! -e $HOME/.dircolors.256dark_bkg; then
-  mv $HOME/.dircolors.256dark $HOME/.dircolors.256dark_bkg || return 1;
-fi
-
-if test -e $HOME/.shell_prompt.sh -a \! -e $HOME/.shell_prompt_bkg.sh; then
-  mv $HOME/.shell_prompt.sh $HOME/.shell_prompt_bkg.sh || return 1;
-fi
-
-if test -e $HOME/.vimrc -a \! -e $HOME/.vimrc_bkg; then
-  mv $HOME/.vimrc $HOME/.vimrc_bkg || return 1;
-fi
-
-if test -e $HOME/.vim -a \! -e $HOME/.vim_bkg; then
-  mv $HOME/.vim $HOME/.vim_bkg || return 1;
-fi
-
-if test $(hostname -d) = "lps.ufrj.br"; then
-  if test -e $HOME/.; then
-    mv $HOME/.bashrc $HOME/.bashrc_bkg
-  fi
-  ln -s $HOME/DotFiles/bashrc_lps $HOME/.bashrc
-fi
-
-ln -s $HOME/DotFiles/config $HOME/.ssh/config
-ln -s $HOME/DotFiles/screenrc $HOME/.screenrc
-ln -s $HOME/DotFiles/zshrc $HOME/.zshrc
-ln -s $HOME/DotFiles/dircolors.256dark $HOME/.dircolors.256dark
-ln -s $HOME/DotFiles/shell_prompt.sh $HOME/.shell_prompt.sh
-ln -s $HOME/DotFiles/shell_prompt_no_pl.sh $HOME/.shell_prompt_no_pl.sh
-
-# It is missing the +lua vim installation
-ln -s $HOME/DotFiles/vimrc $HOME/.vimrc
-
-ln -s $HOME/DotFiles/vim $HOME/.vim
-
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/DotFiles/vim/bundle/Vundle.vim > /dev/null 2> /dev/null || true
-vim \+PluginInstall \+qall
+vim -E -c VundleInstall -c qa
 #/bin/sh -c "vim -E -u NONE -S $HOME/DotFiles/vim/vundle.vim \+PluginInstall \+qall > /dev/null 2> /dev/null" &
 
